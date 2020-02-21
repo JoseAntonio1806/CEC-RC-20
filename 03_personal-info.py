d4f34b4d9d22c8e63ec5f6cd4ede25efb247575d
JoseAntonio1806 <61295449+JoseAntonio1806@users.noreply.github.com>
2020-02-20 22:40:41 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,8 @@
+FirstName = input("Cual es tu primer nombre? ")
+LastName = input("Cual es tu apellido? ")
+Location = input("Donde vives? ")
+Age = input("Cual es tu edad? ")
+space = " "
+print("El usuario se llama" + space + FirstName + space + LastName + space
+      + "tiene" + space + Age + space + "años" + space + "y vive en el sector de"+
+      space+Location+".")
