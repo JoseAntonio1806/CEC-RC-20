6b44e972b9f08397da2d67adf04762b5dfddbe94
JoseAntonio1806 <61295449+JoseAntonio1806@users.noreply.github.com>
2020-02-22 09:12:06 -0500

    Add files via upload

--- sumadedias.py
+++ sumadedias.py
@@ -1,8 +1,8 @@
 def isYearLeap(year):
     if year % 4 == 0  and  (year % 100 != 0  or  year % 400 == 0):
-        return 1
+        return True
     else:
-        return 0
+        return False
 
 def daysInMonth(year, month):
     if isYearLeap(year)==1:
@@ -16,15 +16,16 @@ def daysInMonth(year, month):
     else:
         return None
 
-testYears = [1900, 2000, 2016, 1987]
-testMonths = [2, 2, 1, 11]
-testResults = [28, 29, 31, 30]
-for i in range(len(testYears)):
-	yr = testYears[i]
-	mo = testMonths[i]
-	print(yr, mo, "->", end="")
-	result = daysInMonth(yr, mo)
-	if result == testResults[i]:
-		print("OK")
-	else:
-		print("Failed")
+def dayOfYear(year, month, day):
+    if isYearLeap(year)==1:
+        Dias = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        suma=sum(Dias[0:month-1])+day
+        return suma
+        
+    
+    if isYearLeap(year)==0:
+        Dias = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        suma=sum(Dias[0:month-1])+day
+        return suma
+
+print(dayOfYear(2000, 12, 31))
